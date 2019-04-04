cb5fc31b4746613a1f569af742623f4327f6a862
Luke Cusolito <Luke.Cusolito@Outlook.com>
2019-04-05 07:03:59 +1100

    Initial commit

---
+++ OutaHat/OutaHat/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using OutaHat.Models;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace OutaHat
+{
+    public partial class MainPage : ContentPage
+    {
+        #region Fields
+        private List<HatItem> hatItems = new List<HatItem>();
+        #endregion
+
+        #region Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        private void OnAddClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(itemEntry.Text))
+            {
+                hatItems.Add(new HatItem { Text = itemEntry.Text });
+                itemEntry.Text = string.Empty;
+                displayHatItems.ItemsSource = null; // TODO: Implement property changed event handler
+                displayHatItems.ItemsSource = hatItems;
+            }
+        }
+    }
+}
---
+++ OutaHat/OutaHat/Models/HatItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OutaHat.Models
+{
+    public class HatItem
+    {
+        public string Text { get; set; }
+    }
+}
