4ec3d7e6ee4343d4ca75b3aeb0caf271127961e4
Luke Cusolito <Luke.Cusolito@Outlook.com>
2019-04-05 10:49:04 +1100

    Add ability to remove an item from the list. Add draw functionality

--- OutaHat/OutaHat/MainPage.xaml.cs
+++ OutaHat/OutaHat/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using OutaHat.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace OutaHat
@@ -20,13 +21,44 @@ namespace OutaHat
 
         private void OnAddClicked(object sender, EventArgs e)
         {
+            itemEntry.Focus();
+
             if (!string.IsNullOrWhiteSpace(itemEntry.Text))
             {
-                hatItems.Add(new HatItem { Text = itemEntry.Text });
+                if(!hatItems.Any(x => x.Text.Equals(itemEntry.Text, StringComparison.CurrentCultureIgnoreCase)))
+                    hatItems.Add(new HatItem { Text = itemEntry.Text });
+
                 itemEntry.Text = string.Empty;
+
                 displayHatItems.ItemsSource = null; // TODO: Implement property changed event handler
                 displayHatItems.ItemsSource = hatItems;
             }
+
+        }
+
+        private void OnRemoveItemClicked(object sender, EventArgs e)
+        {
+            var item = (HatItem)((Button)sender).CommandParameter;
+
+            RemoveHatItem(item.Text);
+        }
+
+        private void OnDrawClicked(object sender, EventArgs e)
+        {
+            var rnd = new Random();
+            var index = rnd.Next(hatItems.Count);
+
+            DisplayAlert("Item Drawn", hatItems[index].Text, "OK");
+
+            RemoveHatItem(hatItems[index].Text);
+        }
+
+        private void RemoveHatItem(string itemText)
+        {
+            hatItems.RemoveAll(x => x.Text == itemText);
+
+            displayHatItems.ItemsSource = null; // TODO: Implement property changed event handler
+            displayHatItems.ItemsSource = hatItems;
         }
     }
 }
